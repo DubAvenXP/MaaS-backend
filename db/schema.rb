3b959f0d20dfa2bb8b8122accb0acaa93f68f1f5
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-20 18:34:38 -0600

    feat: services scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_234731) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_003222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_234731) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "deleted_at"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_services_on_client_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -86,4 +98,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_234731) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "availabilities", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "services", "clients"
 end
