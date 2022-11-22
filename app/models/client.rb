c75aafd4e8b42743ebb767ac827239b6c7cbf850
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-22 16:37:26 -0600

    working: services validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,6 @@
 class Client < ApplicationRecord
     has_many :services, dependent: :destroy
 
-    validates :name, presence: true
-    validates :description, presence: true
-
+    validates :name, presence: true, length: 5..100
+	validates :description, presence: true, length: 5..250
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -3,7 +3,14 @@ class Service < ApplicationRecord
 	
 	has_many :shifts, dependent: :destroy
 	has_many :availabilities, dependent: :destroy
+	
+	validate :is_valid_client_id
+	validate :is_valid_date
 
+	validates :name, presence: true, length: 5..100
+	validates :description, presence: true, length: 5..250
+	validates :start_at, presence: true, comparison: { less_than: :end_at, greater_or_equal_than: Date.current }
+	validates :end_at, presence: true
 
 	def show
 		shifts = self.shifts.map do |shifts|
@@ -24,5 +31,29 @@ class Service < ApplicationRecord
 			end_at: self.end_at,
 			shifts: shifts
 		}
+	end
+
+	private
+
+	def is_valid_client_id
+		# validate if client_id exists
+		errors.add("Invalid client id provided") unless Client.find(self.client_id).present?
+	end
+
+	def is_valid_date
+		verify_date_regex = /^(\d{4})(\-)(0?[1-9]|1[012])(\-)(0?[1-9]|[12][0-9]|3[01])$/
+
+		start_at = self.start_at.to_s.split[0]
+		end_at = self.end_at.to_s.split[0]
+
+		errors.add(:start_at, "Date must be formatted correctly") unless end_at.match(verify_date_regex)
+		errors.add(:end_at, "Date must be formatted correctly") unless start_at.match(verify_date_regex)
+		puts "hello world #{start_at.match(verify_date_regex)}"
+		puts "hello world #{"2022-11-22".match(verify_date_regex)}"
+		puts "hello world #{start_at.class}"
+		puts "hello world"
+		puts "hello world"
+		puts "hello world #{end_at.match(verify_date_regex)}"
+		puts "hello world"
 	end
 end
