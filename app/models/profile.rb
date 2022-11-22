b868310f75d46ae234b8e5223e65442a824d25d1
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-22 15:11:33 -0600

    style: indentation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,24 +1,24 @@
 class Profile < ApplicationRecord
 
-  belongs_to :user
+	belongs_to :user
+	has_one_attached :image
 
-  has_one_attached :image
+	# enums
+	enum role: [:engineer, :manager, :admin]
 
-  # enums
-  enum role: [:engineer, :manager, :admin]
+	# callbacks
+	after_create :assign_role
 
-  # callbacks
-  after_create :assign_role
+	
+	def image_url
+		Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true) if self.image.attached?
+	end
 
-  def image_url
-    Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true) if self.image.attached?
-  end
+	private
 
-  private
-
-  # Assigns a default role to a new profile
-  def assign_role
-    self.update(role: :engineer) unless self.role.present?
-  end
+	# Assigns a default role to a new profile
+	def assign_role
+		self.update(role: :engineer) unless self.role.present?
+	end
 
 end
