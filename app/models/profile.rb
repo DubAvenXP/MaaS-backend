15a50461d431bd276b8e0ef9c4f154d88b91a0c5
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-21 18:22:58 -0600

    fix: image validaton

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -11,7 +11,7 @@ class Profile < ApplicationRecord
   after_create :assign_role
 
   def image_url
-    Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true)
+    Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true) if self.image.attached?
   end
 
   private
