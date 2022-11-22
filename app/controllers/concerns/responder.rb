044ff5f71289972ac26558687ecfc58814ec61c2
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-22 15:09:26 -0600

    feat: improvements in responder

--- app/controllers/concerns/responder.rb
+++ app/controllers/concerns/responder.rb
@@ -1,12 +1,19 @@
 module Responder
+	include ErrorUtilities
     extend ActiveSupport::Concern
 
-    def respond_with_not_found(error)
-        render :json => {:errors => payload.errors}, :status => :not_found
+
+    def respond_with_not_found(message)
+		errors = ErrorUtilities::generate_custom_error("id", message)[:errors]
+        render :json => { :errors => errors }, :status => :not_found
     end
 
     def respond_with_errors(payload)
-        render :json => {:errors => payload.errors}, :status => :unprocessable_entity
+		errors = payload.errors
+		errors = ErrorUtilities::format_error(errors) if errors.present?
+		errors = payload[:errors] unless errors.present?
+		
+        render :json => errors, :status => :unprocessable_entity
     end
 
     def respond_with_success(payload, status = :ok)
