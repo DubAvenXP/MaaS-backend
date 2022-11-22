08478014b4a126d90b9a57dfc0717eedee671e24
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-21 19:12:20 -0600

    feat: generate custom error

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -2,21 +2,30 @@ module Utilities
 
     class CustomError
         def initialize()
-            @payload = {
+            @errors = {
                 errors: []
             }
         end
 
+		# generate attributes errors
         def add_error_attribute(attribute, value)
-            @payload[:errors] << {
+            @errors[:errors] << {
                 attribute: attribute,
                 message: "Invalid attribute, #{value} is not valid"
             }
         end
 
-        # getters
-        def get_payload
-            @payload
+		# generate errors with custom message
+		def add_custom_error(attribute, message)
+			@errors[:errors] << {
+                attribute: attribute,
+                message: message
+            }
+		end
+
+        # get all errors
+        def get_errors
+            @errors
         end
     end
 
@@ -28,6 +37,13 @@ module Utilities
             error_handler.add_error_attribute(key, value) if value.nil?
         end
 
-        error_handler.get_payload
+        error_handler.get_errors
     end
+
+	# generate a custom error message
+	def self.generate_custom_error(attribute, message)
+		error_handler = CustomError.new
+		error_handler.add_error_attribute(attribute, message)
+		error_handler.get_errors
+	end
 end
