1cab03667a1a7dba2d6ce6093a0daf3a9550aec6
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-21 18:22:35 -0600

    feat: add responder to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,53 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destroy]
+	before_action :set_user, only: %i[show update destroy]
 
-  def index
-    render json: User.index
-  end
+	# GET /users
+	def index
+		respond_with_success(User.index)
+	end
 
-  def show
-    render json: @user.show
-  end
+	# GET /users/1
+	def show
+		respond_with_success(@user.show)
+	end
 
-  def create
-    @user = User.create(user_params)
+	# POST /users
+	def create
+		@user = User.create(user_params)
 
-    if @user[:errors].present?
-      render json: @user[:errors], status: :unprocessable_entity
-      return
-    end
+		# custom validations
+		return respond_with_errors if @user[:errors].present? 
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+		if @user.save
+			respond_with_success(@user, :created)
+		else
+			respond_with_errors(@user)
+		end
+	end
 
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+	# PATCH/PUT /users/1
+	def update
+		if @user.update(user_params)
+			respond_with_success(@user.show)
+		else
+			respond_with_errors(@user)
+		end
+	end
 
-  def destroy
-    @user.destroy
-  end
+	# DELETE /users/1
+	def destroy
+		@user.destroy
+	end
 
-  private
+	private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+	# Use callbacks to share common setup or constraints between actions.
+	def set_user
+		@user = User.find(params[:id])
+	end
 
-  def user_params
-    params.permit(:email, :password, :password_confirmation, :first_name, :last_name, :role)
-  end
+	# Only allow a list of trusted parameters through.
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :role)
+	end
 end
