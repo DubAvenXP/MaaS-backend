68c4386b0b057a9c5eaf0f1a64eabb7dbedc2444
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-22 15:11:50 -0600

    feat: add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  resources :assignments
-  resources :shifts
-  resources :services
-  resources :clients
-  resources :availabilities
-  resources :profiles, only: %i[update]
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+	resources :assignments
+	resources :shifts
+
+	resources :services do
+		collection do
+			post "assign"
+			get  "assignments"
+		end
+	end
+	
+
+	resources :clients
+	resources :availabilities
+	resources :profiles, only: %i[update]
+	resources :users
+
 end
